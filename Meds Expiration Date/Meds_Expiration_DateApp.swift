c408715ad2d2580693ab13217fdd77edb6b8d888
Kompot1k0 <marruguera@gmail.com>
2023-04-12 12:49:55 +0300

    Initial Commit

---
+++ Meds Expiration Date/Meds_Expiration_DateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meds_Expiration_DateApp.swift
+//  Meds Expiration Date
+//
+//  Created by Admin on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Meds_Expiration_DateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
